aae77b11e0a837030105208558fe3ba63cb51b20
Your Name <you@example.com>
2021-11-02 10:52:45 +0400

    Assignments

---
+++ 01-11-2021/Methods.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_11_2021
+{
+    class Methods
+    {
+        #region Assignment-1 Method
+        public void oneSpace(string text)
+        {
+            while (text.IndexOf("  ") != -1)
+            {
+                text = text.Replace("  ", " ");
+            }
+
+            Console.WriteLine(text);
+
+            //Console.WriteLine(newString);
+
+        }
+        #endregion
+
+        #region Assignment-2 Method
+        public bool checkString(string text)
+        {
+            bool check1 = false;
+            bool check2 = false;
+            bool check3 = false;
+            foreach (var item1 in text)
+            {
+                if (Char.IsUpper(item1) == true)
+                {
+                    check1 = true;
+                    break;
+                }
+
+                else
+                    check1 = false;
+            }
+
+            foreach (var item2 in text)
+            {
+                if (Char.IsLetter(item2) == true)
+                {
+                    check2 = true;
+                    break;
+                }
+
+                else
+                    check2 = false;
+            }
+
+            foreach (var item3 in text)
+            {
+                if (Char.IsDigit(item3) == true)
+                {
+                    check3 = true;
+                    break;
+                }
+
+                else
+                    check3 = false;
+            }
+
+            if (check1 == true && check2 == true && check3 == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Assignment-3 Method
+        public int[] addArray(int[] array, int number)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = number;
+
+            return array;
+        }
+        #endregion
+
+        #region Assignment-4 Method
+        public string[] stringArray(string text, string seperator)
+        {
+            return text.Split(seperator);
+        }
+        #endregion
+
+        #region Assignment-5 Method
+        public string[] LastStringArray()
+        {
+            string[] texts = { "p201 proqramlasdirma oyrenir", "elnur is coni" };
+            string[] lastStringArr = new string[0];
+
+            foreach (var item in texts)
+            {
+                string[] wordItem = item.Split(" ");
+                string lastWord = wordItem[wordItem.Length - 1];
+                Array.Resize(ref lastStringArr, lastStringArr.Length + 1);
+                lastStringArr[lastStringArr.Length - 1] = lastWord;
+            }
+
+            return lastStringArr;
+        }
+        #endregion
+    }
+}
---
+++ 01-11-2021/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _01_11_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods method = new Methods();
+            #region Assignment-1
+            //Console.Write("Text: ");
+            //string text = Console.ReadLine();
+            //method.oneSpace(text);
+            #endregion
+
+            #region Assignment-2
+            //string text2;
+            //Console.Write("String: ");
+            //text2 = Console.ReadLine();
+
+            //if (method.checkString(text2) == true)
+            //{
+            //    Console.WriteLine("Duzgundur!");
+            //}
+
+            //if (method.checkString(text2) == false)
+            //{
+            //    Console.WriteLine("Yanlisdir!");
+            //}
+
+
+            #endregion
+
+            #region Assignment-3
+            //string number;
+            //int numberInt;
+
+            //int[] intArray = { 12, 34, 58, 21 };
+
+
+            //do
+            //{
+            //    Console.Write("Number: ");
+            //    number = Console.ReadLine();
+            //} while (!int.TryParse(number, out numberInt));
+
+
+            //foreach (var item in method.addArray(intArray, numberInt))
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            #endregion
+
+            #region Assignment-4 
+            //Console.Write("Text: ");
+            //string text4 = Console.ReadLine();
+
+            //foreach (var strings in method.stringArray(text, " "))
+            //{
+            //    Console.WriteLine($"{strings} ");
+            //}
+            #endregion
+
+            #region Assigment-5 
+
+            //foreach (var item in method.LastStringArray())
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+        }
+
+
+
+
+    }
+    
+}
